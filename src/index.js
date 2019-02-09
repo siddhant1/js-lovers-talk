07818e0944c3732b5422b17667170a2c0e5c207c
siddhant1 <sidhant.manchanda@gmail.com>
2019-02-09 17:19:45 +0530

    Fixed Stles

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import "./styles.css";
 
 function App() {
   return (
-    // <div className="App">
-    //   <img
-    //     style={{ height: "150px" }}
-    //     src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png"
-    //   />
-    //   <h1>Flash Intro to React Hooks</h1>
-    //   <h2>@siddhant6969</h2>
-    // </div>
-    <Counter />
+    <div className="App container2">
+      <img
+        style={{ height: "150px" }}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png"
+      />
+      <h1>Flash Intro to React Hooks</h1>
+
+      <h2>@siddhant6969</h2>
+      {/* <Counter /> */}
+    </div>
   );
 }
 
